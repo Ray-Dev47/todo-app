3dc2d93c123ea88a207bb2a70f3ca51ba99b361e
Raymond Idengeli <ray.idengeli@gmail.com>
2021-06-06 17:03:00 +0100

    basic functions achieved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Todos from './Todos'
+import AddTodo from './AddTodo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    todos: [
+       {id: 1, content: 'Get a Job'},
+       {id: 2, content: 'Buy a House'}
+    ]
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo]
+    this.setState({
+       todos: todos
+    })
+  }
+  deleteTodo = (id) => {
+      let todos = this.state.todos.filter(todo => {
+          return todo.id !== id
+      }); 
+      this.setState({
+        todos: todos
+     })
+  }
+   render(){
+      return (
+         <div className="todo-app container">
+           <h1 className="center blue-text">Todo's</h1>
+             <AddTodo addTodo={this.addTodo}/>
+             <Todos  todos={this.state.todos}  deleteTodo={this.deleteTodo}/>
+         </div>
+      )
+   }
 }
 
 export default App;
---
+++ src/Todos.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+const Todos = ({todos, deleteTodo}) => {
+
+    const todoList = todos.length ? (
+            todos.map(todo => {
+                return (
+                    <div className="collection-item" key={todo.id}>
+                        <span>{todo.content}</span>
+                        <button onClick={() => {deleteTodo(todo.id)}}>Delete Todo</button>
+                    </div>
+                )
+            }) 
+        ) : (
+            <p className="center">You have no todo's left,  Yay!</p>
+        )
+        return (
+            <div className="todos collection">
+                {todoList}
+            </div>
+        )
+}
+
+export default Todos
